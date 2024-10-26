c248e408e00d7ecda9bfdccf83e0499ee6eea751
luisariass <ariasl@utb.edu.co>
2024-10-26 11:04:45 -0500

    Add DbController class to handle MongoDB connection and user repository

---
+++ infrastructure/repositories/dbcontroller.py
@@ -0,0 +1,8 @@
+from pymongo import MongoClient
+from infrastructure.repositories.user_repository import UserRepository
+class DbController:
+    def __init__(self):
+        client = MongoClient("mongodb+srv://Miche17:aumasemo32@cluster0.6ojhz7l.mongodb.net/?tls=true")
+        self.db = client["RestaurantChain"]
+
+        self.user = UserRepository(self.db)
