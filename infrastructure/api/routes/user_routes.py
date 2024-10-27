e9e5a5b0a6be4966ff94b77df9bb4a34096d4ec7
luisariass <ariasl@utb.edu.co>
2024-10-27 01:43:59 -0500

    Refactor user_routes.py to include user registration, login, update, delete, and get routes

--- infrastructure/api/routes/user_routes.py
+++ infrastructure/api/routes/user_routes.py
@@ -1,22 +1,43 @@
-from http.client import HTTPException
-from os import stat
-from typing import Optional
-from fastapi import APIRouter, Body, Request, Form
-from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
+# infrastructure/api/routes/user_routes.py
+from fastapi import APIRouter, Depends
+from services.user_service import UserService
 from domain.entities.user import User
-from infrastructure.repositories.user_repository import UserRepository
 from infrastructure.repositories.dbcontroller import DbController
-from domain.entities.user import MicroserviceUsuarios
+from domain.use_cases.user_use_cases import UserUseCases
 
-router = APIRouter(prefix="/api/v1/usuarios", tags=["Usuarios"])
-dbcontroller = DbController()
-service = UserRepository(dbcontroller.db)
-micro = MicroserviceUsuarios(service)
+router = APIRouter()
 
+# Crear instancias necesarias para la inyección de dependencias
+db_controller = DbController()
+user_use_cases = UserUseCases(db_controller.user_repo)
+user_service = UserService(user_use_cases)
 
-@router.post("/register/")
-def registrar_usuario(usuario: User):
-    if not micro.register_user(usuario):
+@router.post("/register")
+def register_user(user: User):
+    if not user_service.register_user(user):
         return {"message": "El usuario ya existe"}
     return {"message": "Usuario registrado exitosamente"}
+
+@router.post("/login")
+def login_user(email: str, password: str):
+    user = user_service.authenticate_user(email, password)
+    if user is None:
+        return {"message": "Credenciales incorrectas"}
+    return user
+
+@router.put("/update")
+def update_user(email: str, updated_user: User):
+    user_service.update_user(email, updated_user)
+    return {"message": "Usuario actualizado exitosamente"}
+
+@router.delete("/delete")
+def delete_user(email: str):
+    user_service.delete_user(email)
+    return {"message": "Usuario eliminado exitosamente"}
+
+@router.get("/get")
+def get_user_by_email(email: str):
+    user = user_service.get_user_by_email(email)
+    if user is None:
+        return {"message": "Usuario no encontrado"}
+    return user
