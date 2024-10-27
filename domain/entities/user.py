c5f9537c3d0f3f50ff951c0e6c27cb2f2f740c4d
luisariass <ariasl@utb.edu.co>
2024-10-27 01:43:46 -0500

    Refactor User class to include user_id field

--- domain/entities/user.py
+++ domain/entities/user.py
@@ -1,37 +1,8 @@
-from typing import List
-from infrastructure.repositories.user_repository import UserRepository
+# domain/entities/user.py
 from pydantic import BaseModel, EmailStr
 
 class User(BaseModel):
+    user_id: str
     username: str
-    password: str
     email: EmailStr
-
-class MicroserviceUsuarios:
-    def __init__(self, usuarios_repo: UserRepository):
-        self.usuarios_repo = usuarios_repo
-
-    def register_user(self, user: User) -> bool:
-        return self.usuarios_repo.register_user(user.username, user.password, user.email)
-
-    def authenticate_user(self, email: str, password: str) -> User:
-        user_data = self.usuarios_repo.authenticate_user(email, password)
-        if user_data is None:
-            return None
-        return User(**user_data)
-
-    def actualizar_usuario_por_correo(self, email: str, updated_user: User) -> None:
-        user_data = updated_user.dict()
-        self.usuarios_repo.update_usuario(email, user_data)
-
-    def delete_user(self, email: str) -> None:
-        self.usuarios_repo.eliminar_usuario(email)
-
-    def get_user_by_email(self, email: str) -> User:
-        user_data = self.usuarios_repo.get_user_by_email(email)
-        if user_data is None:
-            return None
-        return User(**user_data)
-    
-    def get_all_programacion(self) -> List[dict]:
-        return self.programacion_repo.get_all_programacion()
+    password: str
