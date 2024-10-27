6d56f839ccee906d44f4145b08828006ee5bf371
luisariass <ariasl@utb.edu.co>
2024-10-27 01:43:29 -0500

    Refactor root route to remove unnecessary response class and add user registration route to user_routes.py

--- main.py
+++ main.py
@@ -1,27 +1,11 @@
+# main.py
+from fastapi import FastAPI
 import uvicorn
-from fastapi import FastAPI, Request
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import HTMLResponse
-
-
 from infrastructure.api.routes import user_routes
+
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-
-@app.get("/")
-async def read_root(request: Request):
-    return {"message": "Hello World"}
-
-app.include_router(user_routes.router)
-
+app.include_router(user_routes.router, prefix="/users", tags=["users"])
 
 if __name__ == "__main__":
     uvicorn.run('main:app')
