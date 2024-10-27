3e08b3e9f5d3e4f87cbf894d290ca3a87cfa35b9
luisariass <ariasl@utb.edu.co>
2024-10-27 01:44:20 -0500

    Refactor UserRepository import in dbcontroller.py

--- infrastructure/repositories/dbcontroller.py
+++ infrastructure/repositories/dbcontroller.py
@@ -1,8 +1,9 @@
+# infrastructure/repositories/dbcontroller.py
 from pymongo import MongoClient
 from infrastructure.repositories.user_repository import UserRepository
+
 class DbController:
     def __init__(self):
         client = MongoClient("mongodb+srv://Miche17:aumasemo32@cluster0.6ojhz7l.mongodb.net/?tls=true")
         self.db = client["RestaurantChain"]
-
-        self.user = UserRepository(self.db)
+        self.user_repo = UserRepository(self.db)
