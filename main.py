979837d0cfe6f03eb4772803cfaebbbbad38867c
luisariass <ariasl@utb.edu.co>
2024-10-29 11:33:13 -0500

    Add inventory routes to main.py

--- main.py
+++ main.py
@@ -2,10 +2,13 @@
 from fastapi import FastAPI
 import uvicorn
 from infrastructure.api.routes import user_routes
+from infrastructure.api.routes import inventory_routes
 
 app = FastAPI()
 
 app.include_router(user_routes.router, prefix="/users", tags=["users"])
+app.include_router(inventory_routes.router, prefix="/inventory", tags=["inventory"])
+
 
 if __name__ == "__main__":
     uvicorn.run('main:app')
