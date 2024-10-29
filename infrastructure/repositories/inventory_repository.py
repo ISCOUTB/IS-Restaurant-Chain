e3c7e87434ed7be50cbc1c2b0921fdde4010ca16
luisariass <ariasl@utb.edu.co>
2024-10-29 12:03:40 -0500

    Refactor InventoryRepository to use product_id instead of ObjectId

--- infrastructure/repositories/inventory_repository.py
+++ infrastructure/repositories/inventory_repository.py
@@ -1,4 +1,3 @@
-from bson.objectid import ObjectId
 from domain.entities.inventory import Inventory
 
 class InventoryRepository:
@@ -6,28 +5,34 @@ class InventoryRepository:
         self.collection = db["Inventario"]
 
     def get_inventory(self, product_id: int) -> Inventory:
-        inventory_data = self.collection.find_one({"_id": ObjectId(product_id)})
+        inventory_data = self.collection.find_one({"product_id": product_id})
         if inventory_data:
             return Inventory(**inventory_data)
         return None
 
     def update_inventory(self, product_id: int, inventory: Inventory) -> bool:
         result = self.collection.update_one(
-            {"_id": ObjectId(product_id)},
+            {"product_id": product_id},
             {"$set": inventory.dict()}
         )
         return result.modified_count > 0
 
-    def create_inventory(self, inventory: Inventory) -> Inventory:
-        inventory_data = inventory.dict()
-        inventory_data["_id"] = ObjectId(inventory_data["product_id"])
+    def create_inventory(self, product_id:int, name:str, 
+                                                    stock:int, price:float, 
+                                                    description ) -> Inventory:
+        inventory_data = {
+            "product_id": product_id,
+            "name": name,
+            "stock": stock,
+            "price": price,
+            "description": description}
         self.collection.insert_one(inventory_data)
-        return inventory
+        return True
 
     def delete_inventory(self, product_id: int) -> bool:
-        result = self.collection.delete_one({"_id": ObjectId(product_id)})
+        result = self.collection.delete_one({"product_id": product_id})
         return result.deleted_count > 0
 
     def last(self) -> Inventory:
-        inventory_data = self.collection.find().sort("_id", -1).limit(1).next()
+        inventory_data = self.collection.find().sort("product_id", -1).limit(1).next()
         return Inventory(**inventory_data)
