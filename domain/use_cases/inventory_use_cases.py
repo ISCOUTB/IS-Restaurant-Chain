a19cb7232eb30208e68aef65039408fee711b99d
luisariass <ariasl@utb.edu.co>
2024-10-29 11:33:54 -0500

    Add InventoryUseCases to handle inventory operations

---
+++ domain/use_cases/inventory_use_cases.py
@@ -0,0 +1,18 @@
+from domain.entities.inventory import Inventory
+from infrastructure.repositories.inventory_repository import InventoryRepository
+
+class InventoryUseCases:
+    def __init__(self, inventory_repo: InventoryRepository):
+        self.inventory_repo = inventory_repo
+
+    def add_inventory(self, inventory: Inventory) -> Inventory:
+        return self.inventory_repo.create_inventory(inventory)
+
+    def get_inventory(self, product_id: int) -> Inventory:
+        return self.inventory_repo.get_inventory(product_id)
+
+    def update_inventory(self, product_id: int, inventory: Inventory) -> bool:
+        return self.inventory_repo.update_inventory(product_id, inventory)
+
+    def delete_inventory(self, product_id: int) -> bool:
+        return self.inventory_repo.delete_inventory(product_id)
