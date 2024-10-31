2cef4c99f94ea6639cb42ef05d882ea3ab861e92
Michse017 <severichecastellano@gmail.com>
2024-10-31 11:36:44 -0500

    Eliminando sistema de pagos

--- main.py
+++ main.py
@@ -3,13 +3,11 @@ from fastapi import FastAPI
 import uvicorn
 from infrastructure.api.routes import user_routes
 from infrastructure.api.routes import inventory_routes
-from infrastructure.api.routes import payment_routes
 
 app = FastAPI()
 
 app.include_router(user_routes.router, prefix="/users", tags=["users"])
 app.include_router(inventory_routes.router, prefix="/inventory", tags=["inventory"])
-app.include_router(payment_routes.router, prefix="/pagos", tags=["pagos"])
 
 
 
