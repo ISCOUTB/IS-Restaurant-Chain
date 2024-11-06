24a433b54619888bdab2697e1fb53abe6b203f12
luisariass <ariasl@utb.edu.co>
2024-11-06 12:19:45 -0500

    Remove unused PaymentRepository import from DbController

--- infrastructure/repositories/dbcontroller.py
+++ infrastructure/repositories/dbcontroller.py
@@ -2,7 +2,6 @@
 from pymongo import MongoClient
 from infrastructure.repositories.user_repository import UserRepository
 from infrastructure.repositories.inventory_repository import InventoryRepository
-from infrastructure.repositories.payment_repository import PaymentRepository
 
 class DbController:
     def __init__(self):
@@ -10,4 +9,3 @@ class DbController:
         self.db = client["RestaurantChain"]
         self.user_repo = UserRepository(self.db)
         self.inventory_repo = InventoryRepository(self.db)
-        self.payment_repo = PaymentRepository(self.db)
