fec1fa04cf393605293f29b742b808bdcb0dbfe3
luisariass <ariasl@utb.edu.co>
2024-11-06 12:19:15 -0500

    Change user_id type from string to int and add UpdateUser model for partial updates

--- domain/entities/user.py
+++ domain/entities/user.py
@@ -1,8 +1,15 @@
 # domain/entities/user.py
 from pydantic import BaseModel, EmailStr
+from typing import Optional
 
 class User(BaseModel):
-    user_id: str
+    user_id: int
     username: str
     email: EmailStr
     password: str
+
+class UpdateUser(BaseModel):
+    username: Optional[str]
+    email: EmailStr
+    password: Optional[str]
+    
