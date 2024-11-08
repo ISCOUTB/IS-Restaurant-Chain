67603b0e4259a79ffb11f5c51ea28b7bda034446
luisariass <ariasl@utb.edu.co>
2024-11-08 16:26:33 -0500

    Refactor user model to enforce password field validation using Pydantic's Field

--- domain/entities/user.py
+++ domain/entities/user.py
@@ -1,15 +1,14 @@
 # domain/entities/user.py
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, Field
 from typing import Optional
 
 class User(BaseModel):
     user_id: int
     username: str
     email: EmailStr
-    password: str
-
+    password: str = Field(..., min_length=1)
 class UpdateUser(BaseModel):
     username: Optional[str]
     email: EmailStr
-    password: Optional[str]
+    password: str = Field(..., min_length=1)
     
