0240b330c9df28638c0a272d1e3bed7194dd8468
luisariass <ariasl@utb.edu.co>
2024-11-09 13:41:52 -0500

    Add optional fields to Inventory model and create InventoryUpdate model for partial updates

--- domain/entities/inventory.py
+++ domain/entities/inventory.py
@@ -1,7 +1,14 @@
 from pydantic import BaseModel
+from typing import Optional
 class Inventory(BaseModel):
     product_id: int
     name:str
     stock: int
     price: float
-    description: str
+    description: Optional[str] = None
+
+class InventoryUpdate(BaseModel):
+    name: Optional[str]
+    stock: Optional[int]
+    price: Optional[float]
+    description: Optional[str]
