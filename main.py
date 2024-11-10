ec43b28758d39554151d6f6f0c59271865560f8b
luisariass <ariasl@utb.edu.co>
2024-11-10 10:28:06 -0500

    Refactor main.py to consolidate route imports and standardize uvicorn run command

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 # main.py
 from fastapi import FastAPI
 import uvicorn
-from infrastructure.api.routes import user_routes
-from infrastructure.api.routes import inventory_routes
+from infrastructure.api.routes import user_routes, inventory_routes
 
 app = FastAPI()
 
 app.include_router(user_routes.router, prefix="/users", tags=["users"])
 app.include_router(inventory_routes.router, prefix="/inventory", tags=["inventory"])
 
-
-
 if __name__ == "__main__":
-    uvicorn.run('main:app')
+    uvicorn.run("main:app")
