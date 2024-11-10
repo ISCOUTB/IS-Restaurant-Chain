e718d9f6e1c613888cce1e25c6ebf4da1f415818
luisariass <ariasl@utb.edu.co>
2024-11-10 10:28:31 -0500

    Validate stock and price in inventory use cases to ensure they are greater than 0

--- domain/use_cases/inventory_use_cases.py
+++ domain/use_cases/inventory_use_cases.py
@@ -1,34 +1,37 @@
 from domain.entities.inventory import Inventory, InventoryUpdate
 from infrastructure.repositories.inventory_repository import InventoryRepository
 
+
 class InventoryUseCases:
     def __init__(self, inventory_repo: InventoryRepository):
         self.inventory_repo = inventory_repo
 
     def add_inventory(self, inventory: Inventory) -> Inventory:
-        if not inventory.product_id:
-            raise ValueError("Product ID is required")
-        if not inventory.name:
-            raise ValueError("Name is required")
-        if not inventory.stock or inventory.stock != 0:
-            raise ValueError("Stock is required")
-        if not inventory.price or inventory.price != 0:
-            raise ValueError("Price is required")
         if self.inventory_repo.get_inventory(inventory.product_id):
             raise ValueError("Inventory already exists")
-        return self.inventory_repo.create_inventory(inventory.product_id, inventory.name, 
-                                                    inventory.stock, inventory.price, 
-                                                    inventory.description)
+        if inventory.stock < 0:
+            raise ValueError("Stock must be greater than 0")
+        if inventory.price < 0:
+            raise ValueError("Price must be greater than 0")
+        return self.inventory_repo.create_inventory(
+            inventory.product_id, inventory.name, 
+            inventory.stock, inventory.price, 
+            inventory.description
+        )
 
     def get_inventory(self, product_id: int) -> Inventory:
         if not self.inventory_repo.get_inventory(product_id):
             raise ValueError("Inventory does not exist")
         return self.inventory_repo.get_inventory(product_id)
 
-    def update_inventory(self, product_id: int, inventoryUpdate: InventoryUpdate) -> InventoryUpdate:
+    def update_inventory(self, product_id: int, inventory_update: InventoryUpdate) -> InventoryUpdate:
         if not self.inventory_repo.get_inventory(product_id):
             raise ValueError("Inventory does not exist")
-        return self.inventory_repo.update_inventory(product_id, inventoryUpdate)
+        if inventory_update.stock < 0:
+            raise ValueError("Stock must be greater than 0")
+        if inventory_update.price < 0:
+            raise ValueError("Price must be greater than 0")
+        return self.inventory_repo.update_inventory(product_id, inventory_update)
 
     def delete_inventory(self, product_id: int) -> bool:
         if not self.inventory_repo.get_inventory(product_id):
