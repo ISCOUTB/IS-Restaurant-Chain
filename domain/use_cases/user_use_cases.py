2a2a231b8433af644ee3d32f08c209ed73596560
luisariass <ariasl@utb.edu.co>
2024-11-10 10:28:44 -0500

    Enhance user registration and update logic to provide clearer error messages for existing users and enforce unique usernames and emails

--- domain/use_cases/user_use_cases.py
+++ domain/use_cases/user_use_cases.py
@@ -1,21 +1,25 @@
 from domain.entities.user import User, UpdateUser
 from infrastructure.repositories.user_repository import UserRepository
 
+
 class UserUseCases:
     def __init__(self, user_repo: UserRepository):
         self.user_repo = user_repo
 
     def register_user(self, user: User) -> User:
-        if not self.user_repo.user_exists(user.user_id):
-            raise ValueError("User ID exists")
-        if not self.user_repo.username_exists(user.username):
-            raise ValueError("Username exists")
-        if not self.user_repo.email_exists(user.email):
-            raise ValueError("Email exists")
-        return self.user_repo.register_user(user.user_id,
-                                            user.username, 
-                                            user.password, 
-                                            user.email)
+        if self.user_repo.user_exists(user.user_id):
+            raise ValueError("User already exists")
+        if self.user_repo.username_exists(user.username):
+            raise ValueError("Username already in use")
+        if self.user_repo.email_exists(user.email):
+            raise ValueError("Email already in use")
+
+        return self.user_repo.register_user(
+            user.user_id,
+            user.username,
+            user.email,
+            user.password
+        )
 
     def authenticate_user(self, email: str, password: str) -> User:
         user_data = self.user_repo.authenticate_user(email, password)
@@ -23,9 +27,13 @@ class UserUseCases:
             return None
         return User(**user_data)
 
-    def update_user(self, user_id: str, updated_user: UpdateUser) -> UpdateUser:
+    def update_user(self, user_id: float, updated_user: UpdateUser) -> UpdateUser:
         if not self.user_repo.user_exists(user_id):
-            raise ValueError("User does not exist")
+            raise ValueError("User already exists")
+        if self.user_repo.username_exists(updated_user.username):
+            raise ValueError("Username already in use")
+        if self.user_repo.email_exists(updated_user.email):
+            raise ValueError("Email already in use")
         return self.user_repo.update_user(user_id, updated_user)
 
     def delete_user(self, email: str) -> None:
