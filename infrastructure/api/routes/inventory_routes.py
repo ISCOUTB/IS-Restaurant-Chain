b52664176befcaa19651f8a2aa167bcbff79d773
luisariass <ariasl@utb.edu.co>
2024-11-10 10:28:52 -0500

    Refactor inventory_routes.py to consolidate imports and standardize variable naming conventions

--- infrastructure/api/routes/inventory_routes.py
+++ infrastructure/api/routes/inventory_routes.py
@@ -1,5 +1,4 @@
-from fastapi import APIRouter
-from fastapi import HTTPException
+from fastapi import APIRouter, HTTPException
 from interfaces.inventory_service import InventoryService
 from domain.entities.inventory import Inventory, InventoryUpdate
 from infrastructure.repositories.dbcontroller import DbController
@@ -7,8 +6,9 @@ from domain.use_cases.inventory_use_cases import InventoryUseCases
 
 router = APIRouter()
 db_controller = DbController()
-Inventory_use_cases = InventoryUseCases(db_controller.inventory_repo)
-inventory_service = InventoryService(Inventory_use_cases)
+inventory_use_cases = InventoryUseCases(db_controller.inventory_repo)
+inventory_service = InventoryService(inventory_use_cases)
+
 
 @router.post("/add")
 def add_inventory(inventory: Inventory):
@@ -18,6 +18,7 @@ def add_inventory(inventory: Inventory):
         raise HTTPException(status_code=400, detail=str(e))
     return {"message": "Inventario añadido exitosamente"}
 
+
 @router.get("/get/{product_id}")
 def get_inventory(product_id: int):
     inventory = inventory_service.get_inventory(product_id)
@@ -25,17 +26,19 @@ def get_inventory(product_id: int):
         raise HTTPException(status_code=404, detail="Inventario no encontrado")
     return inventory
 
+
 @router.put("/update/{product_id}")
-def update_inventory(product_id: int, inventoryUpdate: InventoryUpdate):
+def update_inventory(product_id: int, inventory_update: InventoryUpdate):
     try:
-        inventory_service.update_inventory(product_id, inventoryUpdate)
+        inventory_service.update_inventory(product_id, inventory_update)
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
     return {"message": "Inventario actualizado exitosamente"}
 
+
 @router.delete("/delete/{product_id}")
 def delete_inventory(product_id: int):
-    try: 
+    try:
         inventory_service.delete_inventory(product_id)
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
