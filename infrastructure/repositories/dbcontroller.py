3288e71f75d4a41c481c912adfa9914b1d816f25
luisariass <ariasl@utb.edu.co>
2024-11-25 00:44:57 -0500

    Add OrderRepository and AdminRepository to DbController

--- infrastructure/repositories/dbcontroller.py
+++ infrastructure/repositories/dbcontroller.py
@@ -2,7 +2,8 @@
 from pymongo import MongoClient
 from infrastructure.repositories.user_repository import UserRepository
 from infrastructure.repositories.inventory_repository import InventoryRepository
-
+from infrastructure.repositories.order_repository import OrderRepository
+from infrastructure.repositories.admin_repository import AdminRepository
 
 class DbController:
     def __init__(self):
@@ -12,3 +13,5 @@ class DbController:
         self.db = client["RestaurantChain"]
         self.user_repo = UserRepository(self.db)
         self.inventory_repo = InventoryRepository(self.db)
+        self.order_repo = OrderRepository(self.db)
+        self.admin_repo = AdminRepository(self.db)
