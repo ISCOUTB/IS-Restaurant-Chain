2105dd71b81288e689ffacdc407fd13b2663722c
luisariass <ariasl@utb.edu.co>
2024-11-25 00:44:50 -0500

    Implement admin authentication logic in AdminRepository

--- infrastructure/repositories/admin_repository.py
+++ infrastructure/repositories/admin_repository.py
@@ -1,4 +1,18 @@
 class AdminRepository:
-    def authenticate_admin(self, admin_id: str, password: str) -> dict:
-        pass
+    def __init__(self, db):
+        self.collection = db["Admin"]
+
+    def authenticate_admin(self, admin_id: float, password: str) -> dict:
+        admin = self.collection.find_one({"admin_id": admin_id})
+        if admin is None:
+            return None
+        stored_password = admin["password"]
+        if password != stored_password:
+            return None
+        return {
+            "admin_id": admin["admin_id"],
+            "password": admin["password"],
+            "username": admin["username"],
+        }
+
     
