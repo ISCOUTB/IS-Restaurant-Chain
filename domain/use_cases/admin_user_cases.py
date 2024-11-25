a272ddcd42c3c1964876991d6f049de05735dd6f
luisariass <ariasl@utb.edu.co>
2024-11-25 00:44:37 -0500

    Refactor admin authentication logic to use AdminRepository in AdminUseCases class

--- domain/use_cases/admin_user_cases.py
+++ domain/use_cases/admin_user_cases.py
@@ -1,8 +1,12 @@
 from domain.entities.admin import Admin
 from infrastructure.repositories.admin_repository import AdminRepository
 
-def authenticate_admin(self, admin_id: str, password: str) -> Admin:
-        admin_data = self.user_repo.authenticate_admin(admin_id, password)
-        if admin_data is None:
-            return None
-        return Admin(**admin_data)
+class AdminUseCases:
+    def __init__(self, admin_repo: AdminRepository):
+        self.admin_repo = admin_repo
+        
+    def authenticate_admin(self, admin_id: str, password: str) -> Admin:
+            admin_data = self.admin_repo.authenticate_admin(admin_id, password)
+            if admin_data is None:
+                return None
+            return Admin(**admin_data)
