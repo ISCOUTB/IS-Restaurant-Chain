332a492ae23cf2f0ec68e3fb3f1598c29b4ebc1c
luisariass <ariasl@utb.edu.co>
2024-11-25 01:59:41 -0500

    refactor inventory routes to use async functions and add endpoint to retrieve all inventory items

--- infrastructure/api/routes/inventory_routes.py
+++ infrastructure/api/routes/inventory_routes.py
@@ -9,35 +9,36 @@ db_controller = DbController()
 inventory_use_cases = InventoryUseCases(db_controller.inventory_repo)
 inventory_service = InventoryService(inventory_use_cases)
 
-
 @router.post("/add")
-def add_inventory(inventory: Inventory):
+async def add_inventory(inventory: Inventory):
     try:
         inventory_service.add_inventory(inventory)
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
     return {"message": "Inventario añadido exitosamente"}
 
+@router.get("/get")
+async def get_inventory():
+    inventory = inventory_service.get_all_inventory()
+    return inventory
 
 @router.get("/get/{product_id}")
-def get_inventory(product_id: int):
+async def get_inventory_by_id(product_id: int):
     inventory = inventory_service.get_inventory(product_id)
     if inventory is None:
         raise HTTPException(status_code=404, detail="Inventario no encontrado")
     return inventory
 
-
 @router.put("/update/{product_id}")
-def update_inventory(product_id: int, inventory_update: InventoryUpdate):
+async def update_inventory(product_id: int, inventory_update: InventoryUpdate):
     try:
         inventory_service.update_inventory(product_id, inventory_update)
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
     return {"message": "Inventario actualizado exitosamente"}
 
-
 @router.delete("/delete/{product_id}")
-def delete_inventory(product_id: int):
+async def delete_inventory(product_id: int):
     try:
         inventory_service.delete_inventory(product_id)
     except ValueError as e:
